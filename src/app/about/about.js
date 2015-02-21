0500ac398d0a25502ba8c3fc43cfaf2920c24421
martin_tesar <martin.tesar@clevermaps.cz>
2015-02-21 22:47:22 +0100

    Customized modules from ngbp to cmBikers.

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -1,4 +1,4 @@
-angular.module( 'ngBoilerplate.about', [
+angular.module( 'cmBikers.about', [
   'ui.router',
   'placeholders',
   'ui.bootstrap'
--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -5,7 +5,7 @@
  * automatically.
  */
 describe( 'home section', function() {
-  beforeEach( module( 'ngBoilerplate.home' ) );
+  beforeEach( module( 'cmBikers.home' ) );
 
   it( 'should have a dummy test', inject( function() {
     expect( true ).toBeTruthy();
